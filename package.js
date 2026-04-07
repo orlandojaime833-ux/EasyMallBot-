0aa5174686e140de07da703473bef9634462f19b
orlandojaime833-ux <orlandojaime833@gmail.com>
2026-04-07 22:22:40 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "easymall-bot",
+  "version": "3.0.0",
+  "description": "EasyMall - Global Sales Platform on Telegram",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "telegraf": "^4.15.3",
+    "express": "^4.18.2",
+    "axios": "^1.6.2",
+    "pg": "^8.11.3",
+    "node-cron": "^3.0.3",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
